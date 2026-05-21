48860d55318035cf83d19788e24dd6caff51646e
JovialPan <jovailpan@gmail.com>
2026-05-21 10:48:27 +0800

    DB更新

--- database/init.sql
+++ database/init.sql
@@ -1,111 +1,106 @@
 CREATE DATABASE PetDB;
 
-USE PetDB; 
+USE PetDB;
+GO
 
-CREATE TABLE Users ( 
-    UserID INT PRIMARY KEY IDENTITY(1,1), 
-    Email NVARCHAR(100) NOT NULL UNIQUE,
-    Password NVARCHAR(255) NOT NULL, 
-    UserName NVARCHAR(50),  
+CREATE TABLE Users (
+    UserID INT IDENTITY(1,1) PRIMARY KEY,
+    Email NVARCHAR(100) NOT NULL,
+    Password NVARCHAR(255) NOT NULL,
+    UserName NVARCHAR(50)
 );
 
 CREATE TABLE Food (
-	FoodID INT PRIMARY KEY IDENTITY(1,1),
+    FoodID INT IDENTITY(1,1) PRIMARY KEY,
     Brand NVARCHAR(50),
-    Flavor NVARCHAR(50),
-    UseFor NVARCHAR(100),  
-    AgeGroup NVARCHAR(50),   
-    BodyType NVARCHAR(50), 
+    Flavor NVARCHAR(100),
+    UseFor NVARCHAR(255),
+    AgeGroup NVARCHAR(50),
+    BodyType NVARCHAR(50),
     PetType NVARCHAR(20),
-	AAFCO BIT,
-	ThirdPartyTest BIT,
+    AAFCO BIT,
+    ThirdPartyTest BIT,
     Calories FLOAT
 );
 
 CREATE TABLE Pets (
-    PetID INT PRIMARY KEY IDENTITY(1,1),
-    UserID INT NOT NULL,                      
-    PetName NVARCHAR(50) NOT NULL,          
-    Gender NVARCHAR(10),        
-    Species NVARCHAR(20),           
-    Birthday DATE,                     
-    Weight DECIMAL(5,2),     
-	FoodID INT,
-	Activity NVARCHAR(20),
-	BodyType NVARCHAR(50),  
-    IsSterilized BIT DEFAULT 0, -- 是否結紮 (0:否, 1:是)
-    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE,
-	FOREIGN KEY (FoodID) REFERENCES Food(FoodID)
-);
-
-CREATE TABLE Costs (
-    CostID INT PRIMARY KEY IDENTITY(1,1),
-    UserID INT,
-    PetID INT,
-	Date DATE,
-    Amount DECIMAL(10,2),
-    Category NVARCHAR(50), 
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (PetID) REFERENCES Pets(PetID)
-);
-
-CREATE TABLE Medical (
-    MedicalID INT PRIMARY KEY IDENTITY(1,1),
-    PetID INT,
-    Date DATE,
-    Category NVARCHAR(50),       
-    Description NVARCHAR(255),
-    FOREIGN KEY (PetID) REFERENCES Pets(PetID)
-);
-
-
-CREATE TABLE Snacks (
-	SnackID INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(50),
-    Calories INT,
-    Gram FLOAT
-);
-
-CREATE TABLE DailyFood (
-    DailyFoodID INT PRIMARY KEY IDENTITY(1,1),
-    Calories INT, 
-    RecordDate DATE DEFAULT GETDATE()
-    FOREIGN KEY (PetID) REFERENCES Pets(PetID)
-);
-
-CREATE TABLE DailyWater (
-    DailyWaterID INT PRIMARY KEY IDENTITY(1,1),
-    WaterML INT,  
-    RecordDate DATE DEFAULT GETDATE()
-    FOREIGN KEY (PetID) REFERENCES Pets(PetID)
-);
-
-CREATE TABLE DailySummary (
-    SummaryID INT PRIMARY KEY IDENTITY(1,1),
-    PetID INT,
+    PetID INT IDENTITY(1,1) PRIMARY KEY,
+    UserID INT NOT NULL,
+    PetName NVARCHAR(50) NOT NULL,
+    Gender NVARCHAR(10),
+    Species NVARCHAR(20),
+    Birthday DATE,
+    Weight DECIMAL(5,2),
     FoodID INT,
-    WaterID INT,
-    GoalAchieved BIT,
-    RecordDate DATE DEFAULT GETDATE(),
-    FOREIGN KEY (PetID) REFERENCES  Pets(PetID),   
-    FOREIGN KEY (FoodID) REFERENCES DailyFood(id),
-    FOREIGN KEY (WaterId) REFERENCES DailyWater(id)
-);
+    Activity NVARCHAR(20),
+    BodyType NVARCHAR(50),
+    IsSterilized BIT,
+    RecommendCalories float,
+    RecommendWater float
 
-CREATE TABLE Events (
-    EventID INT PRIMARY KEY IDENTITY(1,1),
-    UserID INT,
-    PetID INT,            
-    EventDate DATE,       
-    EventTime NVARCHAR(10), 
-    Title NVARCHAR(255), 
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (PetID) REFERENCES Pets(PetID) 
+    FOREIGN KEY (UserID)
+        REFERENCES Users(UserID),
+
+    FOREIGN KEY (FoodID)
+        REFERENCES Food(FoodID)
 );
 
 CREATE TABLE Hospitals (
-    HospitalID INT PRIMARY KEY IDENTITY(1,1), 
-    Name NVARCHAR(100) NOT NULL,      
-    Address NVARCHAR(200) NOT NULL,   
-    Phone NVARCHAR(20) NOT NULL       
+    HospitalID INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Address NVARCHAR(200) NOT NULL,
+    Phone NVARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Medical (
+    MedicalID INT IDENTITY(1,1) PRIMARY KEY,
+    PetID INT,
+    Date DATE,
+    Category NVARCHAR(50),
+    Description NVARCHAR(255),
+
+    FOREIGN KEY (PetID)
+        REFERENCES Pets(PetID)
+);
+
+CREATE TABLE DailyFood (
+    DailyFoodID INT IDENTITY(1,1) PRIMARY KEY,
+    PetID INT,
+    Calories INT,
+    RecordDate DATE,
+
+    FOREIGN KEY (PetID)
+        REFERENCES Pets(PetID)
+);
+
+CREATE TABLE DailyWater (
+    DailyWaterID INT IDENTITY(1,1) PRIMARY KEY,
+    PetID INT,
+    WaterML INT,
+    RecordDate DATE,
+
+    FOREIGN KEY (PetID)
+        REFERENCES Pets(PetID)
+);
+
+CREATE TABLE Events (
+    EventID INT IDENTITY(1,1) PRIMARY KEY,
+    UserID INT,
+    PetID INT,
+    EventDate DATE,
+    EventTime NVARCHAR(15),
+    Title NVARCHAR(255),
+
+    FOREIGN KEY (UserID)
+        REFERENCES Users(UserID),
+
+    FOREIGN KEY (PetID)
+        REFERENCES Pets(PetID)
+);
+
+CREATE TABLE Snacks (
+    SnackID INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50),
+    Calories INT,
+    Gram FLOAT
 );
